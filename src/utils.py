8e41813a86e8b8092e86d2b1f276c846124e84cc
Reggie CUshing <r.cushing@esciencecenter.nl>
2023-06-28 11:27:09 +0200

    clean notebooks

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,32 @@
 import pandas as pd
 import json
+import nltk
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+def combine_dicts_arrays(one, two, unique=False):
+    for key in two.keys():
+        one.setdefault(key, [])
+        one[key] += two[key]
+        if unique:
+            one[key] = list(set(one[key]))
+    return one
+
+def get_part_of_speech(text):
+    ner = {
+        'PERSON': [],
+        'ORGANIZATION': [],
+        'LOCATION':[]
+    }
+    tokens = nltk.word_tokenize(text)
+    pos_tags = nltk.pos_tag(tokens)
+    ner_tags = nltk.ne_chunk(pos_tags)
+    
+    for entity in ner_tags.subtrees():
+        if entity.label() in ['PERSON', 'ORGANIZATION', 'LOCATION']:
+            ner[entity.label()].append(' '.join([leaf[0] for leaf in entity.leaves()]))
+    return (pos_tags, ner)
 
 def get_country_data(data_file, countries, years):
     all_countries_data = pd.read_csv(data_file, dtype={'year': str}, comment='#')
